3b2e5a4da1c78416b44bbef84db125055109150f
Shailendra Gupta <shailendra.gupta@digispice.com>
2019-01-02 21:28:27 +0530

    Initial Commit

---
+++ Heady/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heady
+//
+//  Created by spice on 02/01/19.
+//  Copyright © 2019 spice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
